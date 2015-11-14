74800a092bfc2d44af5370c4b73e0384206e09ff
Patrick <pcnicklaus@gmail.com>
2015-11-14 14:52:03 -0700

    d

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ dotenv.load();
 // *** main dependencies *** //
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
